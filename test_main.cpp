a85da0b1b7d0e9fa037d1be3e58282a6473f1530
fltflt <flt309386909@163.com>
2019-11-27 21:44:32 +0800

    1

--- test_main.cpp
+++ test_main.cpp
@@ -7,6 +7,39 @@ using namespace std;
 #include <string> 
 #include<set>
 
+void test_test()
+{
+	vector<int> A, B;
+	for (int i = 0; i < 10; i++)
+	{
+		A.push_back(i);
+	}
+
+	for (int i = 0; i < 5; i++)
+	{
+		B.push_back(i);
+	}
+	A.swap(B);
+
+	for (vector<int>::iterator iter = A.begin(); iter != A.end(); iter++)
+	{
+		cout << *iter << endl;
+	}
+	A.size();
+	A.reserve(50);
+	A.capacity();
+
+	while (A.size() != A.capacity())
+		A.push_back(0);
+
+	A.push_back(100);
+	//A.insert(A.begin()+2, 10);
+	//A.resize(16);
+	//vector<int>::iterator iter = A.begin();
+	//A.erase(iter,iter+7);
+	//vector<int>::iterator iter1=find(A.begin(), A.end(), 9);
+	//A.erase(iter1);
+}
 void map_operator()
 {
 	map<long long int, string> mapStudent;
@@ -48,43 +81,13 @@ void set_operator()
 	cout<<vec_set.count(10)<<endl;
 
 }
+
+int i = 0;
 int main()
 {
-	set_operator();
+
+	int i = i;
 
 }
 
 
-void test_test()
-{
-	vector<int> A,B;
-	for (int i = 0; i < 10; i++)
-	{
-		A.push_back(i);
-	}
-
-	for (int i = 0; i < 5; i++)
-	{
-		B.push_back(i);
-	}
-	A.swap(B);
-
-	for (vector<int>::iterator iter = A.begin(); iter != A.end(); iter++)
-	{
-		cout << *iter << endl;
-	}
-	A.size();
-	A.reserve(50);
-	A.capacity();
-
-	while (A.size() != A.capacity())
-		A.push_back(0);
-
-	A.push_back(100);
-	//A.insert(A.begin()+2, 10);
-	//A.resize(16);
-	//vector<int>::iterator iter = A.begin();
-	//A.erase(iter,iter+7);
-	//vector<int>::iterator iter1=find(A.begin(), A.end(), 9);
-	//A.erase(iter1);
-}
